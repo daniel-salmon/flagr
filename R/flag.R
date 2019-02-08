1efd1d81a40db1eaec367b29e77b48253ba7337b
COLLEGEPROWLER\dsalmon <dsalmon@niche.com>
2019-02-08 17:53:45 -0500

    Add base of flag object

---
+++ R/flag.R
@@ -0,0 +1,25 @@
+flag <- function() {
+    flagenv <- environment()
+    flagenv$flag_list <- list()
+    flagenv$flag_list$help <- list(name = "help",
+                                   description = "Show usage",
+                                   type = "logical",
+                                   default = FALSE)
+    
+    get_flags <- function() {
+      flagenv$flag_list
+    }
+    
+    add_flag <- function(name = "flagname",
+                         type = "logical",
+                         default = TRUE,
+                         description = "A flag") {
+      flagenv$flag_list[[name]] <- list(name = name,
+                                        type = type,
+                                        default = default,
+                                        description = description)
+    }
+    
+    structure(class = "flag", environment())
+}
+
