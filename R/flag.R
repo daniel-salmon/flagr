d564b42bb391ac58cf4d1d266be6adb82d6fb463
COLLEGEPROWLER\dsalmon <dsalmon@niche.com>
2019-02-14 19:00:43 -0500

    Add help message; Add parser / extractor

--- R/flag.R
+++ R/flag.R
@@ -1,29 +1,73 @@
-flag <- function() {
-    flagenv <- environment()
-    flagenv$flag_list <- list()
-    flagenv$flag_list$help <- list(name = "help",
-                                   description = "Show usage",
-                                   type = "logical",
-                                   default = FALSE)
+flag <- function(program_name = "flagr") {
+  flagenv <- environment()
+  flagenv$args <- commandArgs()
+  flagenv$n_args <- length(flagenv$args)
+  flagenv$flag_list <- list(program_name = program_name)
+  flagenv$flag_list$help <- list(name = "help",
+                                 type = "logical",
+                                 description = "Show usage; -h accepted too",
+                                 default = FALSE)
+  
+  get_flags <- function() {
+    flagenv$flag_list
+  }
+  
+  add_flag <- function(name = "flagname",
+                       type = "logical",
+                       description = "A flag",
+                       default = TRUE) {
+    flagenv$flag_list[[name]] <- list(name = name,
+                                      type = type,
+                                      default = default,
+                                      description = description)
+    x <- extract_flag(name)
+    if (is.null(x)) return(default)
+    return(x)
+  }
+  
+  extract_flag <- function(x) {
+    flag_slots <- which(grepl("^-", flagenv$args))
+    flag_pattern <- paste0("^-(-)?", x, "$")
+    start_idx <- which(grepl(flag_pattern, flagenv$args))
+    if (length(start_idx) == 0) return(NULL)
     
-    get_flags <- function() {
-      flagenv$flag_list
+    end_idx <- flag_slots[which(flag_slots == start_idx) + 1]
+    
+    if (is.na(end_idx)) {
+      return(flagenv$args[start_idx])
+    } else {
+      end_idx <- end_idx - 1
+      return(flagenv$args[start_idx:end_idx])
     }
+  }
+  
+  parse <- function() {
+    if (!is.null(extract_flag("h|(help)"))) return(help())
+    print("No help requested")
+  }
+  
+  help <- function() {
+    flags <- flagenv$flag_list
+    pretty <- paste0("\nUsage of ", flags$program_name, ":\n")
+    idx <- which(names(flags) == "program_name")
     
-    add_flag <- function(name = "flagname",
-                         type = "logical",
-                         default = TRUE,
-                         description = "A flag") {
-      flagenv$flag_list[[name]] <- list(name = name,
-                                        type = type,
-                                        default = default,
-                                        description = description)
-    }
-    
-    parse <- function() {
-      commandArgs()
-    }
-    
-    structure(class = "flag", environment())
+    flag_strings <- vapply(flags[-idx], 
+                           function(flag) {paste0("-", flag$name, " ", flag$type, "\n    ", 
+                                                  flag$description, " (default ", flag$default, ")\n")
+                           },
+                           character(1))
+    flag_string <- paste(flag_strings, collapse = "")
+    cat(paste0(pretty, flag_string, "\n"))
+    stop()
+  }
+  
+  print(flagenv$args)
+  
+  structure(class = "flag", environment())
 }
 
+# Tests
+flagr <- flag()
+slave <- flagr$add_flag(name = "slave")
+print(slave)
+flagr$parse()
