53bddc43d6e27c3e09d1c07d60c22bc37faa20c5
COLLEGEPROWLER\dsalmon <dsalmon@niche.com>
2019-02-18 10:31:27 -0500

    Finish add_flag function

--- R/flag.R
+++ R/flag.R
@@ -1,11 +1,13 @@
 flag <- function(program_name = "flagr", trailingOnly = TRUE) {
   flagenv <- environment()
+  valid_types <- c("character", "complex", "double", "integer", "logical")
+  
   flagenv$args <- commandArgs(trailingOnly)
   flagenv$n_args <- length(flagenv$args)
   flagenv$flag_list <- list(program_name = program_name)
-  flagenv$flag_list$help <- list(name = "help",
+  flagenv$flag_list$help <- list(name = "help (h)",
                                  type = "logical",
-                                 description = "Show usage; -h accepted too",
+                                 description = "Show usage",
                                  default = FALSE)
   
   get_flags <- function() {
@@ -16,25 +18,45 @@ flag <- function(program_name = "flagr", trailingOnly = TRUE) {
                        type = "logical",
                        description = "A flag",
                        default = TRUE) {
+    if (!(type %in% valid_types)) {
+      stop(paste("Data type must be one of:", paste(valid_types, collapse = ",")))
+    }
+    if (!can_conv(value = default, type = type)) {
+      stop(paste("Default value cannot be converted to", type))
+    }
+    
     flagenv$flag_list[[name]] <- list(name = name,
                                       type = type,
                                       default = default,
                                       description = description)
     x <- extract_flag(name)
-    if (is.null(x)) return(default)
-    return(x)
+    if (is.null(x)) return(type_conv(value = default, type = type))
+    
+    pattern_flag <- paste0("^-(-)?", name, "(=)?")
+    value <- gsub(pattern_flag, "", x)
+    value <- value[value != ""]
+    if (length(value) == 0) return(type_conv(value = default, type = type))
+    pattern_true <- "(t(rue)?|1)$"
+    pattern_false <- "(f(alse)?|0)$"
+    
+    if (type == "logical") {
+      value <- gsub(pattern_true, "TRUE", value)
+      value <- gsub(pattern_false, "FALSE", value)
+    }
+    
+    return(type_conv(value = value, type = type))
   }
   
   extract_flag <- function(x) {
-    flag_slots <- which(grepl("^-", flagenv$args))
-    flag_pattern <- paste0("^-(-)?", x, "$")
+    flag_slots <- which(grepl("^-(-)?", flagenv$args))
+    flag_pattern <- paste0("^-(-)?", x)
     start_idx <- which(grepl(flag_pattern, flagenv$args))
     if (length(start_idx) == 0) return(NULL)
     
     end_idx <- flag_slots[which(flag_slots == start_idx) + 1]
     
     if (is.na(end_idx)) {
-      return(flagenv$args[start_idx])
+      return(flagenv$args[start_idx:flagenv$n_args])
     } else {
       end_idx <- end_idx - 1
       return(flagenv$args[start_idx:end_idx])
@@ -43,7 +65,6 @@ flag <- function(program_name = "flagr", trailingOnly = TRUE) {
   
   parse <- function() {
     if (!is.null(extract_flag("h|(help)"))) return(help())
-    print("No help requested")
   }
   
   help <- function() {
@@ -61,13 +82,32 @@ flag <- function(program_name = "flagr", trailingOnly = TRUE) {
     quit()
   }
   
-  print(flagenv$args)
-  
   structure(class = "flag", environment())
+}
+
+can_conv <- function(value = "", type = "character") {
+  func <- eval(parse(text = paste0("as.", type)))
+  return(!any(is.na(suppressWarnings(func(value)))))
+}
+
+type_conv <- function(value = "", type = "character") {
+  if (!can_conv(value, type)) {
+    warning(paste0("Unable to convert value to ", type,
+                   ". Returning as a character instead"))
+    return(as.character(value))
+  }
+  func <- eval(parse(text = paste0("as.", type)))
+  return(func(value))
 }
 
 # Tests
 flagr <- flag()
 slave <- flagr$add_flag(name = "slave")
-print(slave)
+n_loops <- flagr$add_flag(name = "n_loops",
+                          type = "integer",
+                          default = 1,
+                          description = "Number of loops to perform")
 flagr$parse()
+print("Here's what your arguments look like")
+print(slave)
+print(n_loops)
